773396882484f76621c0ace9f7dc5ed453905ba7
GioLor <giorgia.lorengo@gmail.com>
2024-06-21 07:05:48 +0200

    Changing the repo in order to be suited for robotics_project one

---
+++ object_detection.py
@@ -0,0 +1,121 @@
+from ultralytics import YOLO
+import cv2
+
+class Object_Detection():
+    def __init__(self, model, path_to_predictions, save_image=False):
+        self.model = YOLO(model)
+        self.save_image = save_image
+
+        self.detected_objects = None
+
+        if save_image:
+            assert path_to_predictions is not None
+            self.path_to_predictions = path_to_predictions
+
+    def print(self, print_to_terminal=False):
+        """
+        This function creates a formatteed string with all the information contained in self.detected_objects.
+        
+        Arguments:
+            print_to_terminal:  if True, the function directly prints the formatted string to terminal
+        Returns:
+            text_to_print:      a formatted string with all the information provided by the model on the last image predicted
+        """
+
+        text_to_print = "\n********************\n* Objects found:"
+        count = 1
+        for block in self.detected_objects:
+            text_to_print += f"\
+                  * object {count}\n\
+                  |- label: {block[6]}\n\
+                  |- class: {(int(block[5]))}\n\
+                  |- box: {block[:4]}\n\
+                  |- confidence: {block[4]}\n\n\
+                "
+            count += 1
+        text_to_print += "********************"
+
+        if print_to_terminal:
+            print(text_to_print)
+
+        return text_to_print
+
+    def predict(self, image : Image, save_predictions_as, print_to_console=False, height=1080, width=1920, bottom_crop=0, top_crop=0, right_crop=0, left_crop=0):
+        """
+        This function calls the model on the image given, and perform the object detection task.
+
+        Arguments:
+            image:                  an Image object on which perform object detection
+            save_predictions_as:    the (path/)name the predictions files needs to be save as
+            print_to_console:       if True, prints the predicted informations on the terminal
+            height:                 height of the image
+            width:                  width of the image
+            bottom_crop:            crop to do on the bottom part of the image, if needed
+            top_crop:               crop to do on the top part of the image, if needed
+            right_crop:             crop to do on the right part of the image, if needed
+            left_crop:              crop to do on the left part of the image, if needed
+
+        Returns:
+            a list of objects containing 
+            - [0] value of x1
+            - [1] value of y1
+            - [2] value of x2
+            - [3] value of y2
+            - [4] confidence of the model on the preediciton
+            - [5] predicted class, as float
+            - [6] label corresponding to the predicted class
+        """
+
+        #pre-processing of the image (cropped eliminating 370 pixels from the top in order to remote the UR5-arm)
+        cropped_image = image[top_crop:height-bottom_crop, left_crop:width-right_crop]
+        if self.save_image:
+            cv2.imwrite(f"{self.path_to_predictions}/{save_predictions_as}.png", cropped_image)
+
+        #prediction with the model
+        results = self.model(cropped_image, imgsz=640)[0]
+        
+        boxes = results.boxes.xyxy.tolist()
+        classes = results.boxes.cls.tolist()
+        names = results.names
+        confidences = results.boxes.conf.tolist()
+
+        info_file = self.path_to_predictions + "/" + save_predictions_as + "_info.txt"
+        f = open(info_file,"w")
+
+        # Iterate through the results
+        i=1
+        prediction_list = []
+        for box, cls, conf in zip(boxes, classes, confidences):
+            x1, y1, x2, y2 = box
+
+            y1 += 7     # the value 7 is empirically obtained
+            y2 += 7     # the value 7 is empirically obtained
+            
+            name = names[int(cls)]
+
+            if self.save_image:
+                label = f"{name} {conf:.2f}"
+                cv2.rectangle(cropped_image, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 2)
+                cv2.putText(cropped_image, label, (int(x1), int(y1) - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+            x1 += left_crop
+            x2 += left_crop
+            y1 += top_crop
+            y2 += top_crop
+
+            prediction_list.append([x1,y1,x2,y2,conf,cls,name])
+            
+            f.write(self.print(prediction_list))
+            i+=1
+
+        f.close()            
+
+        # Save the resulting image with bounding boxes
+        if self.save_image:
+            prediction_file = f"{self.path_to_predictions}{save_predictions_as}_prediction.jpg"
+            cv2.imwrite(prediction_file, cropped_image)
+
+        if print_to_console:
+            self.print(print_to_terminal=True)
+
+        return prediction_list
---
+++ plot_points.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+from matplotlib import cm
+
+def plot_3D_graph(data, name, path_to_save, open_preview=False):
+
+    x = data[:,0]
+    y = data[:,1]
+    z = data[:,2]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    surf = ax.plot_trisurf(y, x, z, cmap=cm.jet, linewidth=0)
+    fig.colorbar(surf)
+
+    ax.xaxis.set_major_locator(MaxNLocator(5))
+    ax.yaxis.set_major_locator(MaxNLocator(6))
+    ax.zaxis.set_major_locator(MaxNLocator(5))
+
+    fig.tight_layout()
+
+    ax.invert_yaxis()
+    ax.invert_xaxis()
+    ax.invert_zaxis()
+
+    ax.set_xlabel("y")
+    ax.set_ylabel("x")
+    ax.set_zlabel("z")
+
+    ax.view_init(-152, -94) 
+
+    fig.savefig(f'{path_to_save}{name}.png')
+    if open_preview:
+        plt.show()
+
+def plot_3D_graph_v2(data, name, path_to_save, open_preview=False):
+    
+    fig = plt.figure(figsize = (8, 8)) 
+    ax = plt.axes(projection = '3d') 
+    
+    # Data for a three-dimensional line 
+    z = data[:,2]  
+    x = data[:,0] 
+    y = data[:,1] 
+    ax.plot3D(y, x, z, 'green') 
+    ax.set_xlabel("y")
+    ax.set_ylabel("x")
+    ax.set_zlabel("z")
+    
+    ax.view_init(-152, -94) 
+
+    fig.savefig(f'{path_to_save}{name}.png')
+    if open_preview:
+        plt.show()
+
+def plot_2D_graph(data, name, path_to_save, open_preview=False):
+    x = data[:,0]
+    y = data[:,1]
+
+    fig = plt.plot(y,x)
+    plt.xlabel("y")
+    plt.ylabel("x")
+
+    fig.savefig(f'{path_to_save}{name}.png')
+    if open_preview:
+        plt.show()
